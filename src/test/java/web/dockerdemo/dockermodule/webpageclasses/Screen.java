f28b397163e32c6a58c69214c499339987c738c8
Rajsekhar355 <rajasekhar.alimili@devrabbit.com>
2019-02-07 18:19:19 +0530

    dfjdsfgsd

---
+++ src/test/java/web/dockerdemo/dockermodule/webpageclasses/Screen.java
@@ -0,0 +1,42 @@
+package web.dockerdemo.dockermodule.webpageclasses;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import com.utilities.BaseClass;import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+@SuppressWarnings("unused")
+public class Screen extends BaseClass {
+	
+	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"http://devrabbit.com/\",\"report_upload_url\":\"https://192.168.1.142:8080/TAF_Automation_DR/UploadReportFile\",\"project_name\":\"DockerDemo\",\"project_description\":\"desc\",\"project_id\":215,\"module_name\":\"DockerModule\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":356,\"testcase_name\":\"TC1\",\"testcase_id\":428,\"testset_id\":0,\"executed_timestamp\":-949203839,\"browser_type\":\"chrome\"}";
+
+	public static String projectName = "dockerdemo";
+	public WebDriver driver;
+	public ExtentReports reports;
+	public ExtentTest test;
+	public static final int datasetsLength = 1;
+
+	public Screen(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	@FindBy(how = How.XPATH, using = "//HTML/BODY[1]/SECTION[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[5]/DIV[2]/SPAN[1]")	
+	private WebElement	_295989SPAN;
+	public String textSPAN_295989() {
+		waitForExpectedElement(driver, _295989SPAN);		
+		String text = _295989SPAN.getText();
+		_295989SPAN.click();
+		return text;
+	}
+
+	@FindBy(how = How.XPATH, using = "//HTML/BODY[1]/HEADER[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/UL[1]/LI[2]")	
+	private WebElement	_295990LI;
+	public String textLI_295990() {
+		waitForExpectedElement(driver, _295990LI);		
+		String text = _295990LI.getText();
+		_295990LI.click();
+		return text;
+	}
+
+}
---
+++ src/test/java/web/dockerdemo/dockermodule/webtestclasses/TC1.java
@@ -0,0 +1,85 @@
+package web.dockerdemo.dockermodule.webtestclasses;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.configurations.Constants;
+import com.configurations.ExtentConfigurations;
+import web.dockerdemo.dockermodule.webpageclasses.Screen;
+import com.utilities.BaseClass;
+import com.utilities.ConfigFilesUtility;
+import com.utilities.Utilities;
+import org.json.JSONObject;
+
+@SuppressWarnings("unused")
+public class TC1 extends BaseClass {
+	ExtentReports reports;
+	ExtentTest test;
+	ITestResult result;
+	private Logger logger;
+	private ConfigFilesUtility configFileObj;
+	public boolean isElementDispalyed = false;	public static final int datasetsLength = 1;
+
+	public TC1() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		logger = Logger.getLogger(TC1.class);
+		configFileObj = new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("tc1.properties");
+		reports = ExtentConfigurations.getExtentInstance(reportsPath, projectPath, Screen.projectName);
+		test = reports.startTest("TC1");
+	}
+	
+	public void setUP() throws Exception {
+		String primaryInfo = Screen.primaryInfo;
+		Constants.PRIMARY_INFO = primaryInfo;
+		driver = launchBrowser(new JSONObject(primaryInfo).optString("browser_type"), configFileObj);
+	}
+
+	public void ScreenTest(int i) throws Exception {
+
+	 try{
+		Constants.TOTAL_TC = Constants.TOTAL_TC + 1;
+		Constants.IS_TESTCASE = true; Constants.iS_WEB = true;
+		int datasetScreencount = 1;
+		Screen objScreen = PageFactory.initElements(driver, Screen.class);
+		testLogHeader(test, "Verify Screen page");
+String text1 = objScreen.textSPAN6503528686_295989();
+		if(text1.equalsIgnoreCase(configFileObj.getProperty("6503528686"+ i + datasetScreencount))){
+			printSuccessLogAndReport(test, logger,  "Validating Text is displayed as : " + configFileObj.getProperty("6503528686"+ i + datasetScreencount));
+		} else {
+			printFailureLogAndReport(test, logger,  "Label Text is not displayed as : " + configFileObj.getProperty("6503528686"+ i + datasetScreencount));
+		}
+String text2 = objScreen.textLI_295990();
+		if(text2.equalsIgnoreCase(configFileObj.getProperty(""+ i + datasetScreencount))){
+			printSuccessLogAndReport(test, logger,  "Validating Text is displayed as : " + configFileObj.getProperty(""+ i + datasetScreencount));
+		} else {
+			printFailureLogAndReport(test, logger,  "Label Text is not displayed as : " + configFileObj.getProperty(""+ i + datasetScreencount));
+		}
+
+	   } catch (Exception e) {
+		  isElementDispalyed = false;
+		  printFailureLogAndReport(test, logger,  "Element is not found" + e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void screensTest() throws Exception {
+		isElementDispalyed = true;
+		for(int datasets = 1; datasets <= Screen.datasetsLength; datasets++) {
+			if(!isElementDispalyed) return;			
+			setUP();
+			if(isElementDispalyed) { ScreenTest(datasets);}
+			tearDown();
+		}	}
+
+	
+	public void tearDown() throws Exception {
+		reports.endTest(test);
+		reports.flush();
+		driver.quit();
+	}
+}
